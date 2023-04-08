8063ad5ebf59c5264de65a85aa657dff3fe29772
cs-mks <113540633+cs-mks@users.noreply.github.com>
2023-04-08 10:32:39 +0530

    Delete Part_1_Assignment directory

--- Part_1_Assignment/Assignment_1/convertor/fahrenheit.scala
+++
@@ -1,7 +0,0 @@
-object Fahrenheit extends App{
-         
-  def convert(celsius:Double)=celsius*1.8000+32.00;
-  println("fahrenheit value for "+ 35 +" celsius is "+convert(35));
-}
-
-
--- Part_1_Assignment/Assignment_1/volume/VolumeSphere.scala
+++
@@ -1,7 +0,0 @@
-object VolumeSphere extends App{
-       
-      def volume(r:Double)=4.0/3.0*math.Pi*r*r*r;
-
-      println(volume(5))
-
-}
